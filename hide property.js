708a2b9d2ca1b932d3fe4e8a8475b46367dff45a
kamankily <kamankily@gmail.com>
2022-03-16 17:56:31 +0700

    Add files via upload

---
+++ hide property.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name hide property info
+// @description  hide property info in vault
+// @author       Tik
+// @match        https://www.torn.com/properties.php#/p=options&tab=vault*
+// @grant        none
+// ==/UserScript==
+
+
+
+(function(){
+    'use strict';
+
+    var delay = 1500
+    var cooldown = setInterval(hideIcon, delay);
+    
+    function hideIcon() {
+        var info = $("div.property-info-cont");
+        if(info.length > 0) {
+            //alert("found");
+
+            $(info).hide();
+            $("div.vault-trans-wrap").hide();
+            delay = 7000;
+
+            //clearInterval(cooldown);
+        } 
+        else {
+            delay = 1500;
+            //alert("not found");
+        }
+    }
+
+})();
